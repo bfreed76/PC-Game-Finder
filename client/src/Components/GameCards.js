61a488d4ea8705888ba67e9de205e6452c580e69
Brinton <brintonfoyreed@gmail.com>
2021-07-31 18:16:23 -0700

    Game cards working

--- client/src/Components/GameCards.js
+++ client/src/Components/GameCards.js
@@ -5,9 +5,8 @@ import { Profile } from './Profile'
 
 export const GameCards = ({ gameID, title, thumb, price, cheapestID }) => {
     const [addInfo, setAddInfo] = useState({})
-    // const { steamRatingText, steamRatingCount, metacriticsScore, releaseDate } = addInfo.gameInfo.
-    const { steamRatingText, steamRatingPercent, steamRatingCount, metacriticScore } = addInfo.gameInfo
-    // const { price, date } = addInfo.cheapestPrice
+    const dealLink = "https://www.cheapshark.com/redirect?dealID={" + cheapestID + "}"
+
 
     useEffect(() => { 
         fetchAdditionalInfo()
@@ -16,6 +15,7 @@ export const GameCards = ({ gameID, title, thumb, price, cheapestID }) => {
     const fetchAdditionalInfo = () => {
             fetch("https://www.cheapshark.com/api/1.0/deals?id=" + cheapestID)
             .then((res) => res.json())
+            .then((res) => console.log(res))
             .then((game) => setAddInfo(game))
             .catch((err) => console.log("error =", err))
          } 
@@ -27,21 +27,16 @@ export const GameCards = ({ gameID, title, thumb, price, cheapestID }) => {
             <Card.Img variant="top" src={thumb} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                <p> Metacritic Score: {metacriticScore} </p>
-                <p> Steam Rating: {steamRatingText}, {steamRatingPercent}, count: {steamRatingCount} </p>
-                </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
-                <ListGroupItem>Lowest Price: {price}</ListGroupItem>
-                <ListGroupItem>Cheapest Price Ever: {addInfo.cheapestPrice.price} </ListGroupItem>
-                <ListGroupItem>Cheapest Price Date: {addInfo.cheapestPrice.date}</ListGroupItem>
+                <ListGroupItem>Current Lowest Price: {price}</ListGroupItem>
+                {/* {addInfo.gameInfo.steamRatingText ? <ListGroupItem>Cheapest Price Ever: {addInfo.gameInfo.steamRatingText} </ListGroupItem> : <></>} */}
             </ListGroup>
             <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href={dealLink} >Link to Deal</Card.Link>
             </Card.Body>
             </Card>
+            <br></br>
         </div>
     )
 }
